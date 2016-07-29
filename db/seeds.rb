00849864c3969fe43c961a4c0ef88e164aa2995d
armandoltx <armando@armandoltx.com>
2016-07-29 15:58:10 +1000

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 User.destroy_all
 
 u1 = User.create(:name => 'Chris', :password => 'chicken1', :email => 'chris@gmail.com');
-u2 = User.create(:name => 'Armando', :password => 'chicken1', :email => 'armando@gmail.com');
+u2 = User.create(:name => 'Armando', :password => 'chicken1', :email => 'armando2a2r@gmail.com');
 u3 = User.create(:name => 'Jin', :password => 'chicken1', :email => 'jin@gmail.com');
 u4 = User.create(:name => 'Sally', :password => 'chicken1', :email => 'sally@gmail.com');
 u5 = User.create(:name => 'John', :password => 'chicken1', :email => 'john@gmail.com');
