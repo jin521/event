679c62440a288ce3b4c097b89cfcf578fe452539
armandoltx <armando@armandoltx.com>
2016-08-01 13:19:06 +1000

    trying to use httparty

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'feeds/index'
+
+  get 'feeds/show'
+
   devise_for :users
   root :to => 'pages#home'
   resources :users
   resources :occasions
+  get '/feed' => 'feeds#index'
+
 end
